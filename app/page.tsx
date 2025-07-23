bf8c616a74a2e0c68cb5e6b6402f5750eac5a07f
h0uk0u <mizutanishusuke@gmail.com>
2025-07-23 10:58:43 +0900

    表示順よくわからん

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,20 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import "./homepage.css";
 
 export default function Home() {
   return (
     <div className="main">
-      <header>
-        <Image
+      <div className="top">
+        <p>あ</p>
+      </div>
+      <header className="header">
+        {/**<Image
+          className="top_picture"
           src="/top.png"
           alt={"かわよい"} 
           width={1344}
           height={896}
-        />
+        />*/}
         <h1>音ゲーブログ</h1>
       </header>
     </div>
