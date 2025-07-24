50b1a01eedb70e5f067717702b325a435fde5b65
h0uk0u <mizutanishusuke@gmail.com>
2025-07-24 21:43:06 +0900

    top

--- app/page.tsx
+++ app/page.tsx
@@ -4,19 +4,20 @@ import "./homepage.css";
 export default function Home() {
   return (
     <div className="main">
-      <div className="top">
-        <p>あ</p>
-      </div>
       <header className="header">
-        {/**<Image
-          className="top_picture"
-          src="/top.png"
-          alt={"かわよい"} 
-          width={1344}
-          height={896}
-        />*/}
-        <h1>音ゲーブログ</h1>
+        <p>A</p>
       </header>
+      <div className="top">
+        <div className="top-image">
+          <Image
+            src="/top.png"
+            alt="kawaii"
+            width={1344}
+            height={896}
+            layout="responsive"
+          />
+        </div>
+      </div>
     </div>
   );
 }
