f3cf31374753a3c6c4dbd029a3b287cd67374761
h0uk0u <mizutanishusuke@gmail.com>
2025-07-27 22:52:40 +0900

    futta-

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -6,6 +6,17 @@ export default function Footer(){
     return(
         <div className="footer">
             <h1>砲口の音ゲーブログ</h1>
+            <ul className="list">
+                <li className="item">
+                    <a href="/">TOP</a>
+                </li>
+                <li className="item">
+                    <a href="/article">ARTICLE</a>
+                </li>
+                <li className="item">
+                    <a href="/about">ABOUT</a>
+                </li>
+            </ul>
         </div>
     )
 }
